385f8e2578657010d08fdadd783400e2577193d6
Matheus Braga <matheusgalaxytab@gmail.com>
2026-05-28 15:51:01 -0400

    feat(infra): add migrations

---
+++ src/CoinFlow/CoinFlow.Infrastructure/Persistence/Migrations/20260527205457_FixRefreshTokenUserIdColumn.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoinFlow.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRefreshTokenUserIdColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_refresh_tokens_users_UserId",
+                table: "refresh_tokens");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "refresh_tokens",
+                newName: "user_id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_refresh_tokens_UserId",
+                table: "refresh_tokens",
+                newName: "IX_refresh_tokens_user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_refresh_tokens_users_user_id",
+                table: "refresh_tokens",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_refresh_tokens_users_user_id",
+                table: "refresh_tokens");
+
+            migrationBuilder.RenameColumn(
+                name: "user_id",
+                table: "refresh_tokens",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_refresh_tokens_user_id",
+                table: "refresh_tokens",
+                newName: "IX_refresh_tokens_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_refresh_tokens_users_UserId",
+                table: "refresh_tokens",
+                column: "UserId",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
