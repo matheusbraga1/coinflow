972b422cb04f151eb68715fb0e1b82a1f0fbf568
Matheus Braga <matheusgalaxytab@gmail.com>
2026-05-29 16:33:42 -0400

    feat: add get current user queries

---
+++ src/CoinFlow/CoinFlow.Application/Users/Queries/GetCurrentUser/GetCurrentUserHandler.cs
@@ -0,0 +1,27 @@
+﻿using CoinFlow.Domain.Exceptions.Users;
+using CoinFlow.Domain.Users;
+using MediatR;
+
+namespace CoinFlow.Application.Users.Queries.GetCurrentUser;
+
+public sealed class GetCurrentUserHandler 
+    : IRequestHandler<GetCurrentUserQuery, CurrentUserResponse>
+{
+    private readonly IUserRepository _userRepository;
+
+    public GetCurrentUserHandler(IUserRepository userRepository) => _userRepository = userRepository;
+
+    public async Task<CurrentUserResponse> Handle(GetCurrentUserQuery query, CancellationToken ct)
+    {
+        var user = await _userRepository.GetByIdAsync(query.UserId, ct);
+
+        if (user is null)
+            throw new UserNotFoundException(query.UserId);
+
+        return new CurrentUserResponse(
+            user.Id, 
+            user.Email.Value, 
+            user.Name, 
+            user.CreatedAt);
+    }
+}
---
+++ src/CoinFlow/CoinFlow.Application/Users/Queries/GetCurrentUser/GetCurrentUserQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace CoinFlow.Application.Users.Queries.GetCurrentUser;
+
+public sealed record GetCurrentUserQuery(Guid UserId) : IRequest<CurrentUserResponse>;
